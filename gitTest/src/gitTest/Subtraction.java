29c4e5f72f73b5edda3a5b37b64c28a72fd567b5
rahul bhuktar <ravi106009@gmail.com>
2025-01-23 16:25:53 +0530

    performing subtraction

---
+++ gitTest/src/gitTest/Subtraction.java
@@ -0,0 +1,14 @@
+package gitTest;
+
+public class Subtraction {
+	public static int subNumber(int a,int b)
+	{
+		return a-b;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=  subNumber(80,10);
+		System.out.println(sum);
+	}
+
+}
