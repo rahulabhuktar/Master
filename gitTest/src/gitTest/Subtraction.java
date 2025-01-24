4763ed17c67902b1323f5048a091a61ab653b935
pavanwankhede <118531515+pavanwankhede@users.noreply.github.com>
2025-01-24 15:09:15 +0530

    performed Array operation By pavan

--- gitTest/src/gitTest/Subtraction.java
+++ gitTest/src/gitTest/Subtraction.java
@@ -27,8 +27,20 @@ public class Subtraction {
 		Subtraction s= new Subtraction();
 		s.addNumber();
 		s.mulNumber();
+
+		 int[] arr = new int[5];
 		
-	
+		arr[0]=101;
+		arr[1]=102;
+		arr[2]=103;
+		arr[3]=104;
+		arr[4]=105;
+
+		for(int i =0; i<5; i++) {
+			
+			System.out.println(arr[i]);
+		}
+	   
 	}
 
 }
