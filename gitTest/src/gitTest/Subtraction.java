33b570f885cc488aeb99c2c08b3b6697ff9ba3e5
Bhagyashrik-cell <bhagukamble122@gmail.com>
2025-01-24 14:58:45 +0530

    add mul method

--- gitTest/src/gitTest/Subtraction.java
+++ gitTest/src/gitTest/Subtraction.java
@@ -12,10 +12,16 @@ public class Subtraction {
 		float y=30.5f;
 		System.out.println(i+y);
 	}
-	
+	public void mulNumber()
+	{
+		int i= 50;
+		int y=3;
+		System.out.println(i*y);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sub=  subNumber(80,10);
+		
 		System.out.println(sub);
 
 		Subtraction s= new Subtraction();
